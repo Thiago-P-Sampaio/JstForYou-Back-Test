2803961d72c8b27454793f0aa50a7f1b015f70d5
Thiago P <thiago.p.sampaio07@gmail.com>
2025-03-20 22:23:41 -0300

    :tada: init: Primeiro Commit - JustForYou -

---
+++ src/test/java/mediaapp/com/just4you/Just4youApplicationTests.java
@@ -0,0 +1,13 @@
+package mediaapp.com.just4you;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Just4youApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
