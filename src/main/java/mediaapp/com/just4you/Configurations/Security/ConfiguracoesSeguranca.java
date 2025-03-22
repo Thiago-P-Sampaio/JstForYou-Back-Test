0fb054b690b9507aa32e9f94e87c7f1aefe6991b
Thiago P <thiago.p.sampaio07@gmail.com>
2025-03-22 16:04:06 -0300

    :sparkles: feat: Configurando o CORS

--- src/main/java/mediaapp/com/just4you/Configurations/Security/ConfiguracoesSeguranca.java
+++ src/main/java/mediaapp/com/just4you/Configurations/Security/ConfiguracoesSeguranca.java
@@ -13,10 +13,15 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebSecurity
-public class ConfiguracoesSeguranca {
+public class ConfiguracoesSeguranca implements WebMvcConfigurer {
 
     @Autowired
     FiltroSeguranca filtroSeguranca;
@@ -30,7 +35,9 @@ public class ConfiguracoesSeguranca {
                          .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                          .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                          .requestMatchers(HttpMethod.GET, "/user/buscar").hasRole("USER"))
+
                  .addFilterBefore(filtroSeguranca, UsernamePasswordAuthenticationFilter.class )
+                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                  .build();
      }
 
@@ -44,6 +51,34 @@ public class ConfiguracoesSeguranca {
      public PasswordEncoder CriptSenha (){
         return  new BCryptPasswordEncoder();
      }
+
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://127.0.0.1:5500")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://127.0.0.1:5500");
+        configuration.addAllowedMethod("GET");
+        configuration.addAllowedMethod("POST");
+        configuration.addAllowedMethod("PUT");
+        configuration.addAllowedMethod("DELETE");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
 
 
