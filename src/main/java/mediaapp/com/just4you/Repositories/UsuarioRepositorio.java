fd8d780a412d97b6ce0142d00e724c5abf432b23
Thiago P <thiago.p.sampaio07@gmail.com>
2025-03-21 23:27:40 -0300

    :sparkles: feat: Implementando Repositório (usuário)

--- src/main/java/mediaapp/com/just4you/Repositories/UsuarioRepositorio.java
+++ src/main/java/mediaapp/com/just4you/Repositories/UsuarioRepositorio.java
@@ -2,8 +2,12 @@ package mediaapp.com.just4you.Repositories;
 
 import mediaapp.com.just4you.Entities.EntidadeUsuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UsuarioRepositorio extends JpaRepository<EntidadeUsuario, Long> {
+
+    UserDetails findByEmail(String email);
+
 }
