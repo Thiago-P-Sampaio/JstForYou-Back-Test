0e0e0c9452fe22deb7f8eb3b9f663666a8bd366d
Thiago P <thiago.p.sampaio07@gmail.com>
2025-04-15 11:45:11 -0300

    :sparkles: feat: Definindo os Controller para Preferência | Atualização Controller Usuario

---
+++ src/main/java/mediaapp/com/just4you/Controllers/PreferenciaController.java
@@ -0,0 +1,24 @@
+package mediaapp.com.just4you.Controllers;
+
+import jakarta.validation.Valid;
+import mediaapp.com.just4you.DTOs.AddPreferenciaDTO;
+import mediaapp.com.just4you.DTOs.PreferenciaDTO;
+import mediaapp.com.just4you.Services.PreferenciaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping ("/preferencia")
+public class PreferenciaController {
+
+    @Autowired
+    PreferenciaService preferenciaService;
+
+
+    @PostMapping("/add/{id}")
+    public ResponseEntity addPreferencia(@PathVariable Long id, @RequestBody @Valid PreferenciaDTO dto){
+        return ResponseEntity.ok(preferenciaService.addPreferencia(id, dto));
+
+    }
+}
--- src/main/java/mediaapp/com/just4you/Controllers/UsuarioController.java
+++ src/main/java/mediaapp/com/just4you/Controllers/UsuarioController.java
@@ -3,8 +3,10 @@ package mediaapp.com.just4you.Controllers;
 import jakarta.validation.Valid;
 import mediaapp.com.just4you.Entities.EntidadeUsuario;
 import mediaapp.com.just4you.Repositories.UsuarioRepositorio;
+import mediaapp.com.just4you.Services.UsuarioService;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,12 +18,19 @@ import java.util.List;
 public class UsuarioController {
 
     @Autowired
-    UsuarioRepositorio usuarioRepositorio;
+    UsuarioService usuarioService;
 
 
     @GetMapping("/buscar")
     public ResponseEntity<List<EntidadeUsuario>> buscar(){
-        return ResponseEntity.ok().body(usuarioRepositorio.findAll());
+        return ResponseEntity.ok().body(usuarioService.buscarUsuarios());
+    }
+
+    @GetMapping("/buscar/{id}")
+    public ResponseEntity buscarUsuario(@PathVariable Long id){
+        EntidadeUsuario usuario = usuarioService.buscarUsuario(id);
+        if(usuario != null)return ResponseEntity.ok(usuario);
+        else return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário não encontrado");
     }
 
 
