9240d74a21db63992de3947c729b0498fc7d1b64
Thiago P <thiago.p.sampaio07@gmail.com>
2025-04-15 16:46:51 -0300

    Revertendo commit anterio

--- src/main/java/mediaapp/com/just4you/DTOs/PreferenciaDTO.java
+++ src/main/java/mediaapp/com/just4you/DTOs/PreferenciaDTO.java
@@ -2,54 +2,50 @@ package mediaapp.com.just4you.DTOs;
 
 import mediaapp.com.just4you.Entities.EntidadePreferencias;
 
-public record PreferenciaDTO(Long id, Long usuarioId, String descrica) {
+public class PreferenciaDTO {
 
-    public PreferenciaDTO(EntidadePreferencias entity){
-        this(entity.getId(), entity.getUsuario().getUsuarioId(), entity.getDescricao());
+    private Long id;
+    private Long usuarioId;
+    private String descricao;
+
+    public PreferenciaDTO(Long id, Long usuarioId, String descricao) {
+        this.id = id;
+        this.usuarioId = usuarioId;
+        this.descricao = descricao;
     }
 
-//    private Long id;
-//    private Long usuarioId;
-//    private String descricao;
-//
-//    public PreferenciaDTO(Long id, Long usuarioId, String descricao) {
-//        this.id = id;
-//        this.usuarioId = usuarioId;
-//        this.descricao = descricao;
-//    }
-//
-//    public PreferenciaDTO() {
-//    }
-//
-//    public PreferenciaDTO(EntidadePreferencias entity){
-//        id = entity.getId();
-//        usuarioId = entity.getUsuario().getUsuarioId();
-//        descricao = entity.getDescricao();
-//
-//    }
-//
-//
-//    public Long getId() {
-//        return id;
-//    }
-//
-//    public void setId(Long id) {
-//        this.id = id;
-//    }
-//
-//    public Long getUsuarioId() {
-//        return usuarioId;
-//    }
-//
-//    public void setUsuarioId(Long usuarioId) {
-//        this.usuarioId = usuarioId;
-//    }
-//
-//    public String getDescricao() {
-//        return descricao;
-//    }
-//
-//    public void setDescricao(String descricao) {
-//        this.descricao = descricao;
-//    }
+    public PreferenciaDTO() {
+    }
+
+    public PreferenciaDTO(EntidadePreferencias entity){
+        id = entity.getId();
+        usuarioId = entity.getUsuario().getUsuarioId();
+        descricao = entity.getDescricao();
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(Long usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
 }
