2ecbfd1db8cc2ce9376dc0bfe23e7409da358ccf
Thiago <thiago.p.sampaio07@gmail.com>
2025-08-11 16:36:54 -0300

    :sparkles: Nova entidade Conteudos

--- src/main/java/mediaapp/com/just4you/Entities/EntidadeConteudos.java
+++ src/main/java/mediaapp/com/just4you/Entities/EntidadeConteudos.java
@@ -1,13 +1,52 @@
 package mediaapp.com.just4you.Entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.util.Set;
 
 @Entity
 @Table (name = "tb_conteudos")
 public class EntidadeConteudos {
 
+    @Id
     private Long conteudoId;
     private String titulo;
 
+    //Relacionamentos
+
+    @OneToMany( mappedBy = "conteudo",  cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<EntidadeListaConteudo> listaConteudos;
+
+
+    public EntidadeConteudos() {
+    }
+
+    public EntidadeConteudos(Long conteudoId, String titulo) {
+        this.conteudoId = conteudoId;
+        this.titulo = titulo;
+    }
+
+    public Long getConteudoId() {
+        return conteudoId;
+    }
+
+    public void setConteudoId(Long conteudoId) {
+        this.conteudoId = conteudoId;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public Set<EntidadeListaConteudo> getListaConteudos() {
+        return listaConteudos;
+    }
+
+    public void setListaConteudos(Set<EntidadeListaConteudo> listaConteudos) {
+        this.listaConteudos = listaConteudos;
+    }
 }
