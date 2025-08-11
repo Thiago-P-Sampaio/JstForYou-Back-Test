2d0ed6156b384627daf04588cee9ab0ad8ed14dd
Thiago <thiago.p.sampaio07@gmail.com>
2025-08-11 16:37:15 -0300

    :sparkles: Nova entidade Lista-Usuário

--- src/main/java/mediaapp/com/just4you/Entities/EntidadeListaUsuario.java
+++ src/main/java/mediaapp/com/just4you/Entities/EntidadeListaUsuario.java
@@ -3,6 +3,8 @@ package mediaapp.com.just4you.Entities;
 
 import jakarta.persistence.*;
 
+import java.util.Set;
+
 @Entity
 @Table (name = "tb_lista_usuario")
 public class EntidadeListaUsuario {
@@ -11,4 +13,38 @@ public class EntidadeListaUsuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long listaId;
 
+
+    //Relacionamentos
+
+    @OneToOne
+    @JoinColumn( name = "usuario_id")
+    private EntidadeUsuario usuario;
+
+    @OneToMany(mappedBy = "lista", cascade = CascadeType.ALL, orphanRemoval = true )
+    private Set<EntidadeListaConteudo> listaConteudos;
+
+    //
+
+    public EntidadeListaUsuario() {
+    }
+
+    public EntidadeListaUsuario(Long listaId) {
+        this.listaId = listaId;
+    }
+
+    public Long getListaId() {
+        return listaId;
+    }
+
+    public void setListaId(Long listaId) {
+        this.listaId = listaId;
+    }
+
+    public EntidadeUsuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(EntidadeUsuario usuario) {
+        this.usuario = usuario;
+    }
 }
