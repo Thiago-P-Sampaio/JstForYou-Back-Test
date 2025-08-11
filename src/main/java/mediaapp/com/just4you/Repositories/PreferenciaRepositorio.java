138344c1261cc4357d725cb0eac23f8f1aaf42c8
Thiago <thiago.p.sampaio07@gmail.com>
2025-08-11 11:42:22 -0300

    :bug: Verificando dependências não carregadas

--- src/main/java/mediaapp/com/just4you/Repositories/PreferenciaRepositorio.java
+++
@@ -1,8 +0,0 @@
-package mediaapp.com.just4you.Repositories;
-
-import mediaapp.com.just4you.Entities.EntidadePreferencias;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PreferenciaRepositorio extends JpaRepository
-        <EntidadePreferencias, Long> {
-}
