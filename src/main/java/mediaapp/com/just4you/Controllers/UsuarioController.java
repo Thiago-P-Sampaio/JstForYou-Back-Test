4f37e23461c4ff71e043404b701cbe95280c6950
Thiago <thiago.p.sampaio07@gmail.com>
2025-08-18 11:35:34 -0300

    :broom: alteração de importação devido a organização de pastas

--- src/main/java/mediaapp/com/just4you/Controllers/UsuarioController.java
+++ src/main/java/mediaapp/com/just4you/Controllers/UsuarioController.java
@@ -1,16 +1,12 @@
 package mediaapp.com.just4you.Controllers;
 
-import jakarta.validation.Valid;
 import mediaapp.com.just4you.Entities.EntidadeUsuario;
-import mediaapp.com.just4you.Repositories.UsuarioRepositorio;
-import mediaapp.com.just4you.Services.UsuarioService;
-import org.apache.coyote.Response;
+import mediaapp.com.just4you.Services.domain.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.Instant;
 import java.util.List;
 
 @RestController
