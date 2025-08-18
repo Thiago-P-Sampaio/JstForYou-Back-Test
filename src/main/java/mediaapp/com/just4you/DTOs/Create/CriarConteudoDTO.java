4bd5a7387e2db1fece7516bf0180ebfc099bd1d4
Thiago <thiago.p.sampaio07@gmail.com>
2025-08-18 16:37:38 -0300

    :construction: Implementando o Tipo de midia e o ID(da api externa)

--- src/main/java/mediaapp/com/just4you/DTOs/Create/CriarConteudoDTO.java
+++ src/main/java/mediaapp/com/just4you/DTOs/Create/CriarConteudoDTO.java
@@ -10,11 +10,21 @@ public class CriarConteudoDTO {
     @NotNull
     private String titulo;
 
+    @NotBlank
+    @NotNull
+    private String tipoMedia;
+
+    @NotBlank
+    @NotNull
+    private Long mediaId;
+
     public CriarConteudoDTO() {
     }
 
-    public CriarConteudoDTO(String titulo) {
+    public CriarConteudoDTO(String titulo, String tipoMedia, Long mediaId) {
         this.titulo = titulo;
+        this.tipoMedia = tipoMedia;
+        this.mediaId = mediaId;
     }
 
     public String getTitulo() {
@@ -23,5 +33,21 @@ public class CriarConteudoDTO {
 
     public void setTitulo(String titulo) {
         this.titulo = titulo;
+    }
+
+    public String getTipoMedia() {
+        return tipoMedia;
+    }
+
+    public void setTipoMedia(String tipoMedia) {
+        this.tipoMedia = tipoMedia;
+    }
+
+    public Long getMediaId() {
+        return mediaId;
+    }
+
+    public void setMediaId(Long mediaId) {
+        this.mediaId = mediaId;
     }
 }
