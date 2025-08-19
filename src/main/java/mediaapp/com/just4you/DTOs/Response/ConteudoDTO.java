3c2ced4e873f74d6615034b2e680e45e3667ae5a
Thiago <thiago.p.sampaio07@gmail.com>
2025-08-19 11:16:49 -0300

    :construction: Implementando ENUM e mediaId(API EXTERNA) no DTO

--- src/main/java/mediaapp/com/just4you/DTOs/Response/ConteudoDTO.java
+++ src/main/java/mediaapp/com/just4you/DTOs/Response/ConteudoDTO.java
@@ -1,11 +1,14 @@
 package mediaapp.com.just4you.DTOs.Response;
 
 import mediaapp.com.just4you.Entities.EntidadeConteudos;
+import mediaapp.com.just4you.Entities.TipoMedia;
 
 public class ConteudoDTO {
 
     private Long conteudoId;
     private String titulo;
+    private String mediaTipo;
+    private Long  mediaId;
 
     public ConteudoDTO() {
     }
@@ -13,6 +16,10 @@ public class ConteudoDTO {
     public ConteudoDTO(EntidadeConteudos entity) {
         this.conteudoId = entity.getConteudoId();
         this.titulo = entity.getTitulo();
+        this.mediaId = entity.getMediaId();
+        if (entity.getMedia() != null) {
+            this.mediaTipo = entity.getMedia().getValue();
+        }
     }
 
     public Long getConteudoId() {
@@ -29,5 +36,21 @@ public class ConteudoDTO {
 
     public void setTitulo(String titulo) {
         this.titulo = titulo;
+    }
+
+    public String getMediaTipo() {
+        return mediaTipo;
+    }
+
+    public void setMediaTipo(String meditaTipo) {
+        this.mediaTipo = meditaTipo;
+    }
+
+    public Long getMediaId() {
+        return mediaId;
+    }
+
+    public void setMediaId(Long mediaId) {
+        this.mediaId = mediaId;
     }
 }
