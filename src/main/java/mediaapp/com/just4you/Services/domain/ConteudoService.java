3cd2927ba2ad37d9a641178cf14811e8a672a92e
Thiago <thiago.p.sampaio07@gmail.com>
2025-08-19 11:18:32 -0300

    :construction: Regras de negócio para ConteudoService

--- src/main/java/mediaapp/com/just4you/Services/domain/ConteudoService.java
+++ src/main/java/mediaapp/com/just4you/Services/domain/ConteudoService.java
@@ -10,7 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ConteudoService {
@@ -18,26 +20,26 @@ public class ConteudoService {
     @Autowired
     ConteudosRepositorio conteudosRepositorio;
 
-//    public EntidadeConteudos adicionarConteudo(@RequestBody @Valid CriarConteudoDTO dto){
-//
-//        boolean exists = conteudosRepositorio.findByMediaAndMediaId(TipoMedia.fromValue(dto.getTipoMedia()), dto.getMediaId());
-//        if(exists) throw new IllegalArgumentException("Conteúdo já existe no banco!");
-//
-//        EntidadeConteudos novoConteudo = new EntidadeConteudos();
-//        Optional.ofNullable(dto.getTitulo())
-//                .filter(s -> s.isBlank())
-//                .ifPresent(novoConteudo::setTitulo);
-//
-//        Optional.ofNullable(dto.getTipoMedia())
-//                .filter(s -> !s.isBlank())
-//                .map(TipoMedia::fromValue)
-//                .ifPresent(novoConteudo::setMedia);
-//
-//        novoConteudo.setMediaId(dto.getMediaId());
-//
-//
-//        return conteudosRepositorio.save(novoConteudo);
-//    }
+    public EntidadeConteudos adicionarConteudo(@RequestBody @Valid CriarConteudoDTO dto){
+
+        boolean exists = conteudosRepositorio.findByMediaAndMediaId(TipoMedia.fromValue(dto.getTipoMedia()), dto.getMediaId());
+        if(exists) throw new IllegalArgumentException("Conteúdo já existe no banco!");
+
+        EntidadeConteudos novoConteudo = new EntidadeConteudos();
+        Optional.ofNullable(dto.getTitulo())
+                .filter(s -> s.isBlank())
+                .ifPresent(novoConteudo::setTitulo);
+
+        Optional.ofNullable(dto.getTipoMedia())
+                .filter(s -> !s.isBlank())
+                .map(TipoMedia::fromValue)
+                .ifPresent(novoConteudo::setMedia);
+
+        novoConteudo.setMediaId(dto.getMediaId());
+
+
+        return conteudosRepositorio.save(novoConteudo);
+    }
 
     public void deletarConteudo(Long id){
         EntidadeConteudos conteudoExistente = conteudosRepositorio.findById(id)
@@ -45,6 +47,29 @@ public class ConteudoService {
 
         conteudosRepositorio.deleteById(id);
 
+    }
+    public List<ConteudoDTO> listarConteudos(){
+        return conteudosRepositorio.findAll()
+                .stream()
+                .map(ConteudoDTO::new) // Forma curta de: .map(entidade -> new ConteudoDTO(entidade))
+                .collect(Collectors.toList());
+    }
+
+
+    public ConteudoDTO buscarConteudoPorId(Long id){
+        EntidadeConteudos conteudo = conteudosRepositorio.findById(id)
+                .orElseThrow(() -> new RuntimeException("Conteúdo não encontrado com ID: " + id ));
+
+        return new ConteudoDTO(conteudo);
+    }
+
+    public ConteudoDTO buscarPorMediaComId(Long mediaId, String media){
+       TipoMedia tipoMedia = TipoMedia.fromValue(media);
+        Optional<EntidadeConteudos> conteudoOptional = conteudosRepositorio.findByMediaIdAndMedia( mediaId, tipoMedia);
+
+        return conteudoOptional
+                .map(ConteudoDTO::new)
+                .orElseGet(ConteudoDTO::new);
     }
 
 
