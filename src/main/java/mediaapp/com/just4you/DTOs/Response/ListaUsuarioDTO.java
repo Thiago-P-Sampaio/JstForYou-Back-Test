e7360fcd1499a331e479e3bfd4a9641fccd9027a
Thiago P <thiago.p.sampaio07@gmail.com>
2025-10-11 17:05:04 -0300

    :construction: classe em RECORD DTO -> ListaUsuario

--- src/main/java/mediaapp/com/just4you/DTOs/Response/ListaUsuarioDTO.java
+++ src/main/java/mediaapp/com/just4you/DTOs/Response/ListaUsuarioDTO.java
@@ -1,40 +1,26 @@
 package mediaapp.com.just4you.DTOs.Response;
 
 import mediaapp.com.just4you.Entities.EntidadeListaUsuario;
+import mediaapp.com.just4you.Entities.EntidadeUsuario;
 
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class ListaUsuarioDTO {
+public record ListaUsuarioDTO(
 
-    private Long listaId;
-    private List<ConteudoDTO> conteudos;
-
-    public ListaUsuarioDTO() {
-    }
-
-    public ListaUsuarioDTO(EntidadeListaUsuario entity) {
-        this.listaId = entity.getListaId();
-        this.conteudos =  entity.getListaConteudos() != null ?
-                entity.getListaConteudos().stream()
-                        .map(listaConteudo -> new ConteudoDTO(listaConteudo.getConteudo()))
-                        .collect(Collectors.toList()) : Collections.emptyList();
-    }
-
-    public Long getListaId() {
-        return listaId;
-    }
-
-    public void setListaId(Long listaId) {
-        this.listaId = listaId;
-    }
-
-    public List<ConteudoDTO> getConteudos() {
-        return conteudos;
-    }
-
-    public void setConteudos(List<ConteudoDTO> conteudos) {
-        this.conteudos = conteudos;
+     Long listaId,
+     List<ConteudoDTO> conteudos
+  )
+{
+    public ListaUsuarioDTO(EntidadeListaUsuario entity){
+        this(
+                entity.getListaId(),
+                entity.getListaConteudos() != null ?
+                        entity.getListaConteudos().stream()
+                                .map(listaConteudo -> new ConteudoDTO(listaConteudo.getConteudo()))
+                                .toList() :
+                        Collections.emptyList()
+        );
     }
 }
