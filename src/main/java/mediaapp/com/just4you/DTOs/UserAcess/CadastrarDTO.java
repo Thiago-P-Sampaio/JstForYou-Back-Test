0cc405535765c214491a67dffd5a0efbaa1ff431
Thiago <thiago.p.sampaio07@gmail.com>
2025-10-13 11:36:44 -0300

    :construction: Validações em DTO para cadastro

--- src/main/java/mediaapp/com/just4you/DTOs/UserAcess/CadastrarDTO.java
+++ src/main/java/mediaapp/com/just4you/DTOs/UserAcess/CadastrarDTO.java
@@ -1,16 +1,28 @@
 package mediaapp.com.just4you.DTOs.UserAcess;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import mediaapp.com.just4you.Roles.PermissaoUsuario;
 
 import java.time.Instant;
 import java.time.LocalDate;
 
-public record CadastrarDTO(String nome,
-                           String email,
-                           String senha,
-                           LocalDate dataNascimento,
-                           PermissaoUsuario role,
-                           Instant dataCadastro
+public record CadastrarDTO(
+
+        @NotBlank(message = "O nome do usuário precisa ser informado!")
+        String nome,
+        @NotBlank(message = "O email do usuário precisa ser informado!")
+        @Email
+        String email,
+        @NotBlank(message = "A senha do usuário precisa ser informado!")
+        String senha,
+        @Past(message = "Inconsistência na data de nascimento informada")
+        @NotNull(message = "É necessário informar a data de nascimento do usuário!")
+        LocalDate dataNascimento,
+        PermissaoUsuario role,
+        Instant dataCadastro
 
 ) {
 }
