330e571cbf99b41b0f99c9aa38881b9f5498a89a
Thiago P <thiago.p.sampaio07@gmail.com>
2025-10-15 22:30:29 -0300

    :construction: Anotações de validação do login

--- src/main/java/mediaapp/com/just4you/DTOs/Security/AuntenticacaoDTO.java
+++ src/main/java/mediaapp/com/just4you/DTOs/Security/AuntenticacaoDTO.java
@@ -1,4 +1,13 @@
 package mediaapp.com.just4you.DTOs.Security;
 
-public record AuntenticacaoDTO(String email, String senha) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record AuntenticacaoDTO(
+
+        @Email
+        @NotBlank
+        String email,
+        @NotBlank
+        String senha) {
 }
