6170bf9913e0a6d3ee18d3e413e8c79972272417
Thiago P <thiago.p.sampaio07@gmail.com>
2025-10-17 17:03:29 -0300

    :construction: Status OK => CREATED em Controller

--- src/main/java/mediaapp/com/just4you/Controllers/AvatarController.java
+++ src/main/java/mediaapp/com/just4you/Controllers/AvatarController.java
@@ -10,6 +10,7 @@ import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,7 +26,7 @@ public class AvatarController {
 
     @PostMapping("/add")
     public ResponseEntity<AvatarDTO> adicionarAvatar(@RequestBody @Valid  AdicionarAvatarDTO dto) {
-        return ResponseEntity.ok().body(avatarService.novoAvatar(dto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(avatarService.novoAvatar(dto));
     }
 
     @PutMapping("/edit/{id}")
