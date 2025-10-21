0bd2bf0c0feffd9d677ed57347ef7f5221e8c33c
Thiago P <thiago.p.sampaio07@gmail.com>
2025-10-21 10:18:59 -0300

    :construction: Repassando validações em campo de edição usuário

--- src/main/java/mediaapp/com/just4you/DTOs/Put/EditarUsuario.java
+++ src/main/java/mediaapp/com/just4you/DTOs/Put/EditarUsuario.java
@@ -1,8 +1,13 @@
 package mediaapp.com.just4you.DTOs.Put;
 
+import jakarta.validation.constraints.Email;
+import mediaapp.com.just4you.Validators.annotations.Texto;
+
 public record EditarUsuario(
 
+        @Texto
         String nome,
+        @Email
         String email,
         String senha,
         Long avatar_id
