2747eeaef0f48e3bb5efe5f0e389370734dfe4bb
Thiago P <thiago.p.sampaio07@gmail.com>
2025-10-22 22:00:45 -0300

    :construction: Validação de redefinição de senha

--- src/main/java/mediaapp/com/just4you/DTOs/UserAcess/RedefinirSenha.java
+++ src/main/java/mediaapp/com/just4you/DTOs/UserAcess/RedefinirSenha.java
@@ -1,12 +1,15 @@
 package mediaapp.com.just4you.DTOs.UserAcess;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record RedefinirSenha(
 
         @NotBlank
+        @Size(min = 6, message = "A senha deve ter no mínimo 6 caracteres!")
         String senha,
         @NotBlank
+        @Size(min = 6, message = "A senha deve ter no mínimo 6 caracteres!")
         String confirmarSenha,
         @NotBlank
         String token
